4f54a00bcacf411011a8470c98028e808b7c0ac8
Deepanshu Sahu <116287792+codeonoto@users.noreply.github.com>
2024-08-12 00:42:43 +0530

    fixing signup bug

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const SignUpPage = () => {
   return (
     <main className='flex h-screen w-full items-center justify-center'>
-      <SignUp />
+      <SignUp forceRedirectUrl='/' />
     </main>
   );
 };
